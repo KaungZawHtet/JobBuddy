02fd4355ea5758933e77fb855d0a7b65a19d6751
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2024-06-05 19:08:33 +0700

    pw checked

--- config/db.go
+++ config/db.go
@@ -17,7 +17,7 @@ func AcessDB() (*gorm.DB, error) {
 	dbPassword := os.Getenv("DATABASE_PASSWORD")
 	dbPort := os.Getenv("DATABASE_PORT")
 
-	log.Printf("Connecting to database with: host=%s user=%s dbname=%s port=%s", dbHost, dbUserName, dbName, dbPort)
+	log.Printf("Connecting to database with: host=%s user=%s dbname=%s port=%s password=%s", dbHost, dbUserName, dbName, dbPort, dbPassword)
 
 	//check ssl mode at https://gorm.io/docs/connecting_to_the_database.html
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s TimeZone=Asia/Bangkok", dbHost, dbUserName, dbPassword, dbName, dbPort)
