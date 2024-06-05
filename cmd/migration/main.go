6175fa808399750fde921bd26045b39377fc89e8
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2024-06-05 23:42:41 +0700

    display env file and wd fixed

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"JobBuddy/config"
-
 	"JobBuddy/models/domain"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 
 	println("Hello From Migration")
 
-	errEnvLoading := godotenv.Load("../../.env")
+	// Get the current working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current directory: %v", err)
+	}
+
+	// Construct the absolute path to the .env file
+	envPath := filepath.Join(wd, ".env")
+
+	// Load the .env file
+	errEnvLoading := godotenv.Load(envPath)
 	if errEnvLoading != nil {
-		panic("Error loading .env file")
+		log.Fatalf("Error loading .env file: %v", errEnvLoading)
 	}
 	db, errAccessDB := config.AcessDB()
 
